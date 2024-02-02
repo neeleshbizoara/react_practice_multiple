15119673134367630440eb12c44b6ad16051f568
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-02 18:07:17 +0530

    Revert changes of inline style

--- essentials_practice_styling_react_component/src/components/Header.tsx
+++ essentials_practice_styling_react_component/src/components/Header.tsx
@@ -6,12 +6,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-        color: 'red',
-        'text-align': 'center',
-        margin: '100px 100px',
-        
-      }}>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
