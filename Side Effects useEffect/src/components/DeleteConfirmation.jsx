b511341ff7c2c73e4a2b2f3073f6af19d79a7cda
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-04 16:40:10 +0530

    Added Progress bar

--- Side Effects useEffect/src/components/DeleteConfirmation.jsx
+++ Side Effects useEffect/src/components/DeleteConfirmation.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  
   useEffect(() => {
-    console.log('TIMER SET');
+    console.log("TIMER SET");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
-      console.log('Cleaning up timer');
+      console.log("Cleaning up timer");
       clearTimeout(timer);
     };
   }, [onConfirm]);
@@ -25,6 +29,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER}></ProgressBar>
     </div>
   );
 }
