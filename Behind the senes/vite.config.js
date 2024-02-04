4bf87ebc601892d16a48bff1cfad500465fd2eaa
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-04 19:46:11 +0530

    million.js package is used to optimize react app

--- Behind the senes/vite.config.js
+++ Behind the senes/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+ */
+
+import million from "million/compiler";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+ 
+export default defineConfig({
+  plugins: [million.vite({ auto: true }), react()],
+});
