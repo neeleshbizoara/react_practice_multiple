73cf9976a7eb5ed6c788393f2c6a6f29ed0d8f67
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-06 22:05:58 +0530

    Action inside component

--- Advanced Redux/src/App.js
+++ Advanced Redux/src/App.js
@@ -1,16 +1,80 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
-function App() {
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
 
+let isInitial = true;
+
+function App() {
   const uiStore = useSelector((state) => state.ui);
+  const cartStore = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-367ac-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cartStore) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+
+      const responseData = await response.json();
+    };
+
+    if(isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sent cart data successfully!",
+        })
+      );
+      
+    });
+  }, [cartStore, dispatch]);
 
   return (
-    <Layout>
-      {uiStore.cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {uiStore.cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
