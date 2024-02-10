0a0225154c04e0e02524b96cddd4c2683c70070d
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-10 16:10:47 +0530

    useLoader and loader in route

--- adv-routing-starting-project/frontend/src/pages/Events.js
+++ adv-routing-starting-project/frontend/src/pages/Events.js
@@ -1,35 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
+import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [fetchedEvents, setFetchedEvents] = useState();
+  // const [error, setError] = useState();
 
+  const events = useLoaderData()
   useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        setError('Fetching events failed.');
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
+    // async function fetchEvents() {
+    //   setIsLoading(true);
+    //   setIsLoading(false);
+    // }
+    // fetchEvents();
   }, []);
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
+      <EventsList events={events}  />
+
+      {/* <div style={{ textAlign: 'center' }}>
         {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      </div> */}
+      {/* {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />} */}
     </>
   );
 }
