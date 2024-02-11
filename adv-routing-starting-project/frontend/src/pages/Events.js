f9287b53ec7d36a6a4dbfc48f39ed2624a95cfbd
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-11 14:20:16 +0530

    Throw error uninf json from react-router-dom

--- adv-routing-starting-project/frontend/src/pages/Events.js
+++ adv-routing-starting-project/frontend/src/pages/Events.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 function EventsPage() {
   // const events = useLoaderData();
@@ -21,12 +21,15 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsss");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
     // throw new Error({message: "Could not fetch events."});
-    throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500})
+
+    // throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500});
+
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     // const resData = await response.json();
     // return resData.events;
