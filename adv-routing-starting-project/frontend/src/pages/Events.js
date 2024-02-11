b3eaad541262f37ed42e0cad443fd89fcf61ebce
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-11 10:39:02 +0530

    useLoader

--- adv-routing-starting-project/frontend/src/pages/Events.js
+++ adv-routing-starting-project/frontend/src/pages/Events.js
@@ -1,32 +1,34 @@
 import { useEffect, useState } from "react";
 
 import EventsList from "../components/EventsList";
-// import { useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [fetchedEvents, setFetchedEvents] = useState();
-  // const [error, setError] = useState();
+  // const events = useLoaderData();
+  const data = useLoaderData();
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
 
-  // const events = useLoaderData()
-  useEffect(() => {
-    // async function fetchEvents() {
-    //   setIsLoading(true);
-    //   setIsLoading(false);
-    // }
-    // fetchEvents();
-  }, []);
+  const events = data.events;
   return (
     <>
-      {/* <EventsList events={events} /> */}
-      <EventsList/>
-      {/* <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div> */}
-      {/* {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />} */}
+      <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    return { isError: true, message: "Could not fetch events." };
+  } else {
+    // const resData = await response.json();
+    // return resData.events;
+    // const res = new Response('any data', {status: 201});
+    return response;
+  }
+}
