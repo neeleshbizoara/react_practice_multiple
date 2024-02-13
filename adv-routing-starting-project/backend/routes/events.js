878f441b41d020453bea6eed377fa884d2133c69
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-13 12:52:24 +0530

    useFetcher Hook: Fetching Data on Component Mount: Dynamic Data Loading: Data Refetching: Handling Loading and Error States: Reusable Data Fetching Logic:

--- adv-routing-starting-project/backend/routes/events.js
+++ adv-routing-starting-project/backend/routes/events.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
     const events = await getAll();
     setTimeout(() => {
       res.json({ events: events });
-    }, 1500)
+    }, 2500)
   } catch (error) {
     next(error);
   }
