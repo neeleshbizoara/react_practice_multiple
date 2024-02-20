96b3ffd003fd982c3cbbbeacfe3ac9277e36a91d
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-20 15:40:10 +0530

    Animating Layout Changes

--- Animations/src/components/ChallengeItem.jsx
+++ Animations/src/components/ChallengeItem.jsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
-import {motion} from 'framer-motion';
+import { useContext } from "react";
+import { motion } from "framer-motion";
 
-import { ChallengesContext } from '../store/challenges-context.jsx';
+import { ChallengesContext } from "../store/challenges-context.jsx";
 
 export default function ChallengeItem({
   challenge,
@@ -11,24 +11,24 @@ export default function ChallengeItem({
   const { updateChallengeStatus } = useContext(ChallengesContext);
 
   const formattedDate = new Date(challenge.deadline).toLocaleDateString(
-    'en-US',
+    "en-US",
     {
-      day: '2-digit',
-      month: 'short',
-      year: 'numeric',
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
     }
   );
 
   function handleCancel() {
-    updateChallengeStatus(challenge.id, 'failed');
+    updateChallengeStatus(challenge.id, "failed");
   }
 
   function handleComplete() {
-    updateChallengeStatus(challenge.id, 'completed');
+    updateChallengeStatus(challenge.id, "completed");
   }
 
   return (
-    <li>
+    <motion.li layout>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -43,12 +43,19 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div className={`challenge-item-details ${isExpanded ? 'expanded' : ''}`}>
+        <div
+          className={`challenge-item-details ${isExpanded ? "expanded" : ""}`}
+        >
           <p>
             <button onClick={onViewDetails}>
-              View Details{' '}
+              View Details{" "}
               {/* <span className="challenge-item-details-icon">&#9650;</span> */}
-              <motion.span animate={{rotate: isExpanded ? 180 : 0}} className="challenge-item-details-icon">&#9650;</motion.span>
+              <motion.span
+                animate={{ rotate: isExpanded ? 180 : 0 }}
+                className="challenge-item-details-icon"
+              >
+                &#9650;
+              </motion.span>
             </button>
           </p>
 
@@ -61,6 +68,6 @@ export default function ChallengeItem({
           )}
         </div>
       </article>
-    </li>
+    </motion.li>
   );
 }
