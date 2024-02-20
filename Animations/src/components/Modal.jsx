55453918a7841ba55d55c63ff05730f6b2eb0907
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-20 14:52:34 +0530

    Animation with framer motion using whileFocus, whileHover, transition, animate, initial, animate, exit

--- Animations/src/components/Modal.jsx
+++ Animations/src/components/Modal.jsx
@@ -1,14 +1,22 @@
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
+  const hiddenAnimationState = { opacity: 0, y: 30 };
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        initial={hiddenAnimationState}
+        animate={{ opacity: 1, y: 0 }}
+        exit={hiddenAnimationState}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
