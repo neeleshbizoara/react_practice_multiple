9a1ca9bf84ec93752d0726898b6ea4976aa4861f
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-20 14:55:55 +0530

    Animate modal usinf variant

--- Animations/src/components/Modal.jsx
+++ Animations/src/components/Modal.jsx
@@ -2,14 +2,21 @@ import { createPortal } from "react-dom";
 import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
-  const hiddenAnimationState = { opacity: 0, y: 30 };
+  // const hiddenAnimationState = { opacity: 0, y: 30 };
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={hiddenAnimationState}
-        animate={{ opacity: 1, y: 0 }}
-        exit={hiddenAnimationState}
+        // initial={hiddenAnimationState}
+        // animate={{ opacity: 1, y: 0 }}
+        // exit={hiddenAnimationState}
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >
