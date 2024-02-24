f9641b0a37e2ac7920383128bcb73406e1602a0f
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-24 12:10:41 +0530

    react-typescript todos prop derilling

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,31 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   reportWebVitals(console.log);
-  console.log(process.env)
-  const apiUrl = process.env.REACT_APP_API_ENDPOINT;
+  console.log(process.env);
+
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  // const todoList = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // const apiUrl = process.env.REACT_APP_API_ENDPOINT;
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodoList( (prevTodo) => {
+      return prevTodo.concat(newTodo);
+    })
+  }
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodoList((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId)
+    })
+  }
+
   return (
     <div>
-      <p>API URL: {apiUrl}</p>
+      <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todoList} onRemoveTodo={removeTodoHandler}/>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
+    // <div>
+    //   <p>API URL: {apiUrl}</p>
     // </div>
   );
 }
---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,15 @@
+import Todo from "../models/todo";
+import TodoItems from "./TodoItems";
+import classes from './Todos.module.css';
+
+const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void }> = (props) => {
+  return (
+    <ul className={classes.todos}>
+      {props.items.map((item) => (
+        <TodoItems key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}/>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
