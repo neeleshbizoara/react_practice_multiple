adc35e83b931430fa4dc7250ba0e79c28b96b6e6
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-26 10:18:30 +0530

    Typescript using Context

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,39 +1,13 @@
-import React, { useState } from "react";
-import "./App.css";
-import reportWebVitals from "./reportWebVitals";
-import Todos from "./components/Todos";
-import Todo from "./models/todo";
-import NewTodo from "./components/NewTodo";
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import TodosContextProvider from './store/todos-context';
 
 function App() {
-  reportWebVitals(console.log);
-  console.log(process.env);
-
-  const [todoList, setTodoList] = useState<Todo[]>([]);
-  // const todoList = [new Todo("Learn React"), new Todo("Learn TypeScript")];
-  // const apiUrl = process.env.REACT_APP_API_ENDPOINT;
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodoList( (prevTodo) => {
-      return prevTodo.concat(newTodo);
-    })
-  }
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodoList((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId)
-    })
-  }
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todoList} onRemoveTodo={removeTodoHandler}/>
-    </div>
-    // <div>
-    //   <p>API URL: {apiUrl}</p>
-    // </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
