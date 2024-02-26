a1f6a6e9039c78b7c7eebe049077082c972dbc50
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-26 19:39:43 +0530

    Get api call

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -6,11 +6,34 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [posts, setPostes] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      if(!response.ok) {
+        throw new Error('Some thing wrong');
+        //Need to implement 
+      }
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
-    // debugger;
-    setPostes((existingPost) => {
+    const response = fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPost) => {
       return [postData, ...existingPost];
     });
   }
@@ -21,16 +44,23 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && 
-      <ul className={classes.posts}>
-        {posts.map((item) => (
-          <Post author={item.author} body={item.body} key={Math.random()} />
-        ))}
-      </ul>
-      }
-      {posts.length === 0 && <div style={{textAlign: 'center', color: 'white'}}>
-        <h2>There are no posts yet.</h2>
-        <p>Start adding some!</p></div>}
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((item) => (
+            <Post author={item.author} body={item.body} key={Math.random()} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isFetching && <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>}
     </>
   );
 }
