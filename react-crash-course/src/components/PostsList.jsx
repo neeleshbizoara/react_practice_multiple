102ee63795c5dfc5988536af33abdf0674df0b23
fds-neelesh <neelesh.bizoara@calsoftinc.com>
2024-02-26 18:04:34 +0530

    Add posts using state

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Post from './Post';
-import NewPost from './NewPost';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPostes] = useState([]);
+
+  function addPostHandler(postData) {
+    // debugger;
+    setPostes((existingPost) => {
+      return [postData, ...existingPost];
+    });
+  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
+      {posts.length > 0 && 
       <ul className={classes.posts}>
-        <Post author="Manuel" body="Check out the full course!" />
+        {posts.map((item) => (
+          <Post author={item.author} body={item.body} key={Math.random()} />
+        ))}
       </ul>
+      }
+      {posts.length === 0 && <div style={{textAlign: 'center', color: 'white'}}>
+        <h2>There are no posts yet.</h2>
+        <p>Start adding some!</p></div>}
     </>
   );
 }
